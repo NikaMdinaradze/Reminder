ee3e36cf23fe7ef8d6b0686ada6e2fc5aec01b91
NikaMdinaradze <mdinaradzenika04@gmail.com>
2023-12-28 15:22:19 +0400

    Add tests for register endpoint

--- user/tests.py
+++ user/tests.py
@@ -1,35 +1,45 @@
-# your_app/tests/test_serializers.py
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 from user.serializers import UserCreateSerializer, UserGetSerializer
 
+VALID_DATA = {
+    "username": "gorgasali",
+    "password": "StronkPassword123",
+    "email": "vaxtang.gorgasali@yahoo.com",
+    "first_name": "vaxtangi",
+    "last_name": "farnavaziani",
+}
+
+INVALID_DATA = [
+    {
+        "username": "",
+        "password": "StronkPassword123",
+        "email": "vaxtang.gorgasali@yahoo.com",
+        "first_name": "vaxtangi",
+        "last_name": "farnavaziani",
+    },
+    {
+        "username": "gorgasali",
+        "password": "",
+        "email": "vaxtang.gorgasali@yahoo.com",
+        "first_name": "vaxtangi",
+        "last_name": "farnavaziani",
+    },
+]
+
 
 class UserModelSerializerTest(TestCase):
+    """
+    test class for user's serializers
+    """
+
     def setUp(self):
-        self.valid_data = {
-            "username": "gorgasali",
-            "password": "StronkPassword123",
-            "email": "vaxtang.gorgasali@yahoo.com",
-            "first_name": "vaxtangi",
-            "last_name": "farnavaziani",
-        }
-        self.invalid_datas = [
-            {
-                "username": "",
-                "password": "StronkPassword123",
-                "email": "vaxtang.gorgasali@yahoo.com",
-                "first_name": "vaxtangi",
-                "last_name": "farnavaziani",
-            },
-            {
-                "username": "gorgasali",
-                "password": "",
-                "email": "vaxtang.gorgasali@yahoo.com",
-                "first_name": "vaxtangi",
-                "last_name": "farnavaziani",
-            },
-        ]
+        self.valid_data = VALID_DATA
+        self.invalid_datas = INVALID_DATA
 
     def test_valid_serializer_data(self):
         """
@@ -46,12 +56,7 @@ class UserModelSerializerTest(TestCase):
 
         for invalid_data in self.invalid_datas:
             serializer = UserCreateSerializer(data=invalid_data)
-            try:
-                self.assertFalse(serializer.is_valid())
-            except AssertionError:
-                raise AssertionError(
-                    f"invalid behaviour for provided data:" f"\n {invalid_data}"
-                )
+            self.assertFalse(serializer.is_valid())
 
     def test_serialization(self):
         """
@@ -67,3 +72,32 @@ class UserModelSerializerTest(TestCase):
         }
 
         self.assertEqual(serializer.data, expected_data)
+
+
+class RegisterTest(APITestCase):
+    """
+    test class for user's view
+    """
+
+    url = reverse("user-register")
+
+    def test_register_user(self):
+        """
+        testing register endpoint with valid data
+        """
+        data = VALID_DATA
+
+        response = self.client.post(self.url, data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+
+    def test_register_invalid_data(self):
+        """
+        testing register endpoint with invalid data
+        """
+        invalid_datas = INVALID_DATA
+
+        for invalid_data in invalid_datas:
+            response = self.client.post(self.url, invalid_data, format="json")
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
