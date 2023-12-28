abb950dca80c31d98bc8852e9c19de77ab850450
NikaMdinaradze <mdinaradzenika04@gmail.com>
2023-12-28 16:21:43 +0400

    Add tests for user/me endpoint

--- user/tests.py
+++ user/tests.py
@@ -76,7 +76,7 @@ class UserModelSerializerTest(TestCase):
 
 class RegisterTest(APITestCase):
     """
-    test class for user's view
+    test class for register endpoint
     """
 
     url = reverse("user-register")
@@ -94,10 +94,61 @@ class RegisterTest(APITestCase):
 
     def test_register_invalid_data(self):
         """
-        testing register endpoint with invalid data
+        testing /register/ endpoint with invalid data
         """
         invalid_datas = INVALID_DATA
 
         for invalid_data in invalid_datas:
             response = self.client.post(self.url, invalid_data, format="json")
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class UserMeTest(APITestCase):
+    """
+    test class for /user/me endpoint
+    """
+
+    def setUp(self):
+        self.user = User.objects.create_user(**VALID_DATA)
+        self.url = reverse("user-details")
+
+    def test_get_authenticated_user_info(self):
+        self.client.force_authenticate(user=self.user)
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["username"], self.user.username)
+
+    def test_update_authenticated_user_info(self):
+        self.client.force_authenticate(user=self.user)
+        updated_valid_data = VALID_DATA.copy()
+        updated_valid_data["username"] = "updated_username"
+        data = updated_valid_data
+
+        response = self.client.put(self.url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.username, "updated_username")
+
+    def test_delete_authenticated_user_info(self):
+        self.client.force_authenticate(user=self.user)
+
+        response = self.client.delete(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertFalse(User.objects.filter(id=self.user.id).exists())
+
+    def test_get_unauthenticated_user_info(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_put_invalid_data(self):
+        self.client.force_authenticate(user=self.user)
+        invalid_datas = INVALID_DATA
+
+        for invalid_data in invalid_datas:
+            response = self.client.put(self.url, invalid_data)
             self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- user/views.py
+++ user/views.py
@@ -1,16 +1,17 @@
-from .serializers import UserGetSerializer, UserCreateSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status, permissions
 from django.http import Http404
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import UserCreateSerializer, UserGetSerializer
+
 
 # Create your views here.
 class Register(APIView):
-
-    serializer_class =  UserCreateSerializer
+    serializer_class = UserCreateSerializer
 
     def post(self, request):
-        """ Creates User (Registration)"""
+        """Creates User (Registration)"""
         user = request.data
         serializer = self.serializer_class(data=user)
         if not serializer.is_valid():
@@ -18,10 +19,12 @@ class Register(APIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+
 class UserMe(APIView):
     permission_classes = [permissions.IsAuthenticated]
+
     def get_serializer_class(self):
-        if self.request.method == 'GET':
+        if self.request.method == "GET":
             return UserGetSerializer
         return UserCreateSerializer
 
@@ -34,14 +37,14 @@ class UserMe(APIView):
             raise Http404("User not found or not logged in")
 
     def get(self, request):
-        """ Gets Authenticated User's Info"""
+        """Gets Authenticated User's Info"""
         user = self.get_user(request)
         serializer_class = self.get_serializer_class()
         serializer = serializer_class(user)
         return Response(serializer.data)
 
     def put(self, request):
-        """ Updates Authenticated User's Info"""
+        """Updates Authenticated User's Info"""
         user = self.get_user(request)
         serializer_class = self.get_serializer_class()
         serializer = serializer_class(user, data=request.data)
@@ -51,7 +54,7 @@ class UserMe(APIView):
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def delete(self, request):
-        """ Deletes Authenticated User's Info"""
+        """Deletes Authenticated User's Info"""
         user = self.get_user(request)
         user.delete()
         return Response(status.HTTP_204_NO_CONTENT)
