8704dd4bbf60001e966b1f5afb1519c78d356077
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-01-03 16:46:18 +0400

    Add deadline validation in serializer

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,12 +1,23 @@
+from django.utils import timezone
 from rest_framework import serializers
+
 from .models import ToDo
 
 
 class ToDoSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
+    owner = serializers.ReadOnlyField(source="owner.username")
+
     class Meta:
         model = ToDo
-        fields = ['id', 'title', 'todo', 'created', 'deadline','owner']
+        fields = ["id", "title", "todo", "created", "deadline", "owner"]
+
+    def validate(self, attrs):
+        """
+        Check that the creation time is before the deadline.
+        """
+        if attrs["deadline"] < timezone.now():
+            raise serializers.ValidationError("deadline must be after current time")
+        return attrs
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
